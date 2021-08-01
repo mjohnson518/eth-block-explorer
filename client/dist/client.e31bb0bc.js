c8f55c39d97da76f5bed7697f9785277f1c3b4a7
Marc <marcjohnson518@gmail.com>
2021-08-01 14:23:28 -0600

    3rd

--- client/dist/client.e31bb0bc.js
+++ client/dist/client.e31bb0bc.js
@@ -32436,7 +32436,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64105" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57230" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
